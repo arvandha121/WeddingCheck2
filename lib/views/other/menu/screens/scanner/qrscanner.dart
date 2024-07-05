3650551bcfd1cd901b251604a67f7ab180340c32
arvandha121 <nauvan121@gmail.com>
2024-07-05 12:50:47 +0700

    update notif, flip camera, dan flash camera qrscanner

--- lib/views/other/menu/screens/scanner/qrscanner.dart
+++ lib/views/other/menu/screens/scanner/qrscanner.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:screen_brightness/screen_brightness.dart';
 import 'package:weddingcheck/app/database/dbHelper.dart';
 import 'package:weddingcheck/app/model/listItem.dart';
 
@@ -15,8 +17,19 @@ class QRScanner extends StatefulWidget {
 class _QRScannerState extends State<QRScanner> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   QRViewController? controller;
+  Timer? _inactivityTimer;
+  Timer? _cameraCheckTimer;
 
   bool isFlashOn = false;
+  bool isFrontCamera = false; // Track the current camera
+  bool isCameraPaused = false; // Track if the camera is paused
+
+  @override
+  void initState() {
+    super.initState();
+    _startInactivityTimer();
+    _startCameraCheckTimer();
+  }
 
   @override
   void reassemble() {
@@ -30,20 +43,93 @@ class _QRScannerState extends State<QRScanner> {
   @override
   void dispose() {
     controller?.dispose();
+    _inactivityTimer?.cancel();
+    _cameraCheckTimer?.cancel();
     super.dispose();
   }
 
-  void _toggleFlash() {
+  void _startInactivityTimer() {
+    _inactivityTimer?.cancel();
+    _inactivityTimer = Timer(Duration(minutes: 1), _showInactivityDialog);
+  }
+
+  void _startCameraCheckTimer() {
+    _cameraCheckTimer?.cancel();
+    _cameraCheckTimer = Timer.periodic(Duration(seconds: 10), (timer) {
+      if (isCameraPaused) {
+        // Do not resume the camera here
+      }
+    });
+  }
+
+  void _showInactivityDialog() {
+    controller?.pauseCamera(); // Pause the camera when showing the dialog
+    isCameraPaused = true;
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Inactivity Alert"),
+          content: Text("KETIK OK KETIKA INGIN MENGGUNAKAN QR SCANNER KEMBALI"),
+          actions: <Widget>[
+            TextButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _startInactivityTimer(); // Restart the timer
+                controller?.resumeCamera(); // Resume the camera
+                isCameraPaused = false;
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _resetInactivityTimer() {
+    _startInactivityTimer();
+  }
+
+  void _toggleFlash() async {
     setState(() {
       isFlashOn = !isFlashOn;
     });
     controller?.toggleFlash();
+    _resetInactivityTimer();
+
+    // Adjust screen brightness if using the front camera and flash is on
+    if (isFrontCamera && isFlashOn) {
+      try {
+        await ScreenBrightness()
+            .setScreenBrightness(1.0); // Set brightness to 100%
+      } catch (e) {
+        print("Error setting screen brightness: $e");
+      }
+    } else {
+      try {
+        await ScreenBrightness()
+            .resetScreenBrightness(); // Reset to default brightness
+      } catch (e) {
+        print("Error resetting screen brightness: $e");
+      }
+    }
+  }
+
+  void _flipCamera() {
+    setState(() {
+      isFrontCamera = !isFrontCamera;
+    });
+    controller?.flipCamera();
+    _resetInactivityTimer();
   }
 
   void _onQRViewCreated(QRViewController controller) {
     this.controller = controller;
     controller.scannedDataStream.listen((scanData) {
       controller.pauseCamera();
+      isCameraPaused = true;
       final String scannedData = scanData.code ?? "";
       fetchListItemByGambar(scannedData).then((item) {
         if (item != null) {
@@ -51,65 +137,28 @@ class _QRScannerState extends State<QRScanner> {
             updateListItemStatus(item.id!).then(
               (_) {
                 // Ensure updateListItemStatus accepts only one argument
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text("Konfrimasi"),
-                      content: Text("${item.nama} terdaftar hadir."),
-                      actions: <Widget>[
-                        TextButton(
-                          child: Text("OK"),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                            controller.resumeCamera();
-                          },
-                        ),
-                      ],
-                    );
-                  },
+                _showNotificationDialog(
+                  context,
+                  "Konfirmasi",
+                  "${item.nama} terdaftar hadir.",
+                  Duration(seconds: 5), // 3 seconds delay for success
                 );
               },
             );
           } else {
-            showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text("Ditolak"),
-                  content: Text(
-                      "Akses ditolak karena ${item.nama} telah terdaftar."),
-                  actions: <Widget>[
-                    TextButton(
-                      child: Text("OK"),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                        controller.resumeCamera();
-                      },
-                    ),
-                  ],
-                );
-              },
+            _showNotificationDialog(
+              context,
+              "Ditolak",
+              "Akses ditolak karena ${item.nama} telah terdaftar.",
+              Duration(seconds: 8), // 3 seconds delay for already registered
             );
           }
         } else {
-          showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: Text("Error"),
-                content: Text("Kode QR tidak valid atau item tidak ditemukan."),
-                actions: [
-                  TextButton(
-                    child: Text("OK"),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                      controller.resumeCamera();
-                    },
-                  ),
-                ],
-              );
-            },
+          _showNotificationDialog(
+            context,
+            "Error",
+            "Kode QR tidak valid atau item tidak ditemukan.",
+            Duration(seconds: 12), // 15 seconds delay for error
           );
         }
       });
@@ -149,38 +198,85 @@ class _QRScannerState extends State<QRScanner> {
         false;
   }
 
+  void _showNotificationDialog(
+      BuildContext context, String title, String content, Duration delay) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(content),
+          actions: <Widget>[
+            TextButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                controller?.resumeCamera();
+                isCameraPaused = false;
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+    Future.delayed(delay, () {
+      if (Navigator.of(context).canPop()) {
+        Navigator.of(context).pop(); // Dismiss the dialog
+      }
+      controller?.resumeCamera(); // Resume the camera
+      isCameraPaused = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: _onWillPop,
-      child: Scaffold(
-        body: Stack(
-          children: [
-            QRView(
-              key: qrKey,
-              onQRViewCreated: _onQRViewCreated,
-              overlay: QrScannerOverlayShape(
-                borderColor: Colors.red,
-                borderRadius: 10,
-                borderLength: 30,
-                borderWidth: 10,
-                cutOutSize: 300,
-              ),
-            ),
-            Positioned(
-              right: 10,
-              bottom: 10,
-              child: FloatingActionButton(
-                onPressed: _toggleFlash,
-                child: Icon(
-                  isFlashOn ? Icons.flash_off : Icons.flash_on,
-                  color: Colors.white,
+      child: GestureDetector(
+        onTap: _resetInactivityTimer,
+        onPanDown: (_) => _resetInactivityTimer(),
+        child: Scaffold(
+          body: Stack(
+            children: [
+              QRView(
+                key: qrKey,
+                onQRViewCreated: _onQRViewCreated,
+                overlay: QrScannerOverlayShape(
+                  borderColor: Colors.red,
+                  borderRadius: 10,
+                  borderLength: 30,
+                  borderWidth: 10,
+                  cutOutSize: 300,
                 ),
-                backgroundColor:
-                    isFlashOn ? Colors.yellow[700] : Colors.black54,
               ),
-            ),
-          ],
+              Positioned(
+                left: 10, // Position the flash button on the left
+                bottom: 10,
+                child: FloatingActionButton(
+                  onPressed: _toggleFlash,
+                  child: Icon(
+                    isFlashOn ? Icons.flash_off : Icons.flash_on,
+                    color: Colors.white,
+                  ),
+                  backgroundColor:
+                      isFlashOn ? Colors.yellow[700] : Colors.black54,
+                ),
+              ),
+              Positioned(
+                right: 10, // Position the flip camera button on the right
+                bottom: 10,
+                child: FloatingActionButton(
+                  onPressed: _flipCamera,
+                  child: Icon(
+                    Icons.flip_camera_android,
+                    color: Colors.white,
+                  ),
+                  backgroundColor: Colors.blue,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
